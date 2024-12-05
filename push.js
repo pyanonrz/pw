65b86c22f22cbcd856c693c9f94fe457bf21780d
blackseed <blackseed1234@users.noreply.github.com>
2024-12-05 20:30:20 +0400

    0.04

--- push.js
+++ push.js
@@ -7,6 +7,8 @@ let vapidKey = {
 
 push.setVapidDetails('mailto:test@code.co.uk', vapidKey.publicKey, vapidKey.privateKey);
 
-let sub = {};
+let sub = {
+    "endpoint":"https://fcm.googleapis.com/fcm/send/d6fvbeABmwc:APA91bH4mu3ng4CNYDyB-Zl8FZ0d03W1oMJ4qu_YmtBKw4FcGSEA7ujhDcf1F8ygQ39_EFNsjBROBtcCelD9wnrt7ah0NsqeQvVVWppQzQ-ZP_lz-1TZ0OXtk_Q5NO0MiEtj3mhix0Lu","expirationTime":null,"keys":{"p256dh":"BLJ8TJKpv8f2PXkGCcYjf1VO0EMzal5JlLz-ysrSuBupn86-e3vcz5flyLs-7vXwvJ3d5tPMdwxW_634NVZmRTQ","auth":"hYl23fnvKBvbxUf2LKDq4g"}
+};
 
 push.sendNotification(sub, 'test message')
--- sw.js
+++ sw.js
@@ -1,3 +1,16 @@
-self.addEventListener('push', () => {
-    self.ServiceWorkerRegistration.sendNotification('test message', {})
+self.addEventListener('push', function(e) {
+    var options = {
+        body: 'This notification was generated from a push!',
+        icon: 'images/example.png',
+        vibration: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: '2'
+        },
+        actions: [
+            {action: 'explore', title: 'Explore this new world', icon: 'images/checkmark.png'},
+            {action: 'close', title: 'Close', icon:'images/xmark.png'}
+        ] 
+    };
+    e.waitUntil(self.ServiceWorkerRegistration.sendNotification('Hello world!', options))
 });
