12d0050ee44e2dbd35bcbf33a448765093403153
blackseed <blackseed1234@users.noreply.github.com>
2024-12-05 19:56:38 +0400

    initial

---
+++ push.js
@@ -0,0 +1,8 @@
+var push = require('web-push');
+
+let vapidKey = {
+    publicKey: 'BBZY7Q3KEtZArAAWMLi_qzWHbH4vAoqPpIXnRhmlUaw0PVs1Kt_2fgLhuaVI5i8MWASBKx3d6W6UoH2U3qChw9U',
+    privateKey: 'CZtf_JUxmXkCKbzwaKedPPO9BFC99U2rk-GUYDbYAa8'
+}
+
+console.log(push);
