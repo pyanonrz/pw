6af10412fac27a61f95812fd9e7b4ea8e405bbfa
blackseed <blackseed1234@users.noreply.github.com>
2024-12-05 20:39:42 +0400

    0.06

--- sw.js
+++ sw.js
@@ -12,5 +12,5 @@ self.addEventListener('push', function(e) {
             {action: 'close', title: 'Close', icon:'images/xmark.png'}
         ] 
     };
-    e.waitUntil(self.ServiceWorkerRegistration.showNotification('Hello world!', options))
+    e.waitUntil(self.registration.showNotification('Hello world!', options))
 });
